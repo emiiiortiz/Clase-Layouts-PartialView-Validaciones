f6331bda2177c91aca9e75be667e59e5be734efc
emiortiz1992 <42984124+emiortiz1992@users.noreply.github.com>
2022-05-01 21:58:26 -0300

    first commit

---
+++ Clase-Layouts-PartialView-Validaciones/Clase-Layouts-PartialView-Validaciones/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace Clase_Layouts_PartialView_Validaciones.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string AvatarURL { get; set; }
+        public string Email { get; set; }
+        public int Puntos { get; set; }
+
+
+    }
+}
